694696bfae0827436fbf627410c2041040b3a4f9
Matheus Mello <matheusmello_22@hotmail.com>
2021-07-23 09:14:10 +0100

    Install dependencies and initial configuration files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,24 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'airbnb-typescript', 'prettier', 'prettier/@typescript-eslint', 'prettier/react'],
+  plugins: ['import'],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        alias: {
+          _assets: './src/assets',
+          _components: './src/components',
+          _atoms: './src/components/atoms',
+          _molecules: './src/components/molecules',
+          _organisms: './src/components/organisms',
+          _navigations: './src/navigations',
+          _scenes: './src/scenes',
+          _services: './src/services',
+          _styles: './src/styles',
+          _utils: './src/utils',
+        },
+      },
+    },
+  },
 };
